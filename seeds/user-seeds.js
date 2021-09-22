17dad320c0aee8068cf83fa4cfca7e4e6f62011f
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-22 19:42:13 -0400

    updating id

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,16 +2,19 @@ const { User } = require('../models');
 
 const userData = [
     {
+        id: '1',
         username: 'tim',
         email: 'tim@gmail.com',
         password: 'tim3'
     },
     {
+        id: '2',
         username: 'jim',
         email: 'jim@gmail.com',
         password: 'jim3'
     },
     {
+        id: '3',
         username: 'wim',
         email: 'wim123@gmail.com',
         password: 'wim3'
