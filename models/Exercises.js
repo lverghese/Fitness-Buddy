0954702d7a7711f8e719d60cff79de47bb8fabfc
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-09-22 20:23:20 -0400

    updated the exercises file for linking

--- models/Exercises.js
+++ models/Exercises.js
@@ -1,6 +1,40 @@
 const Workoutplan = require('./Workoutplan');
 const User = require('./User');
 const Goals = require('./Goals');
+const cut = require('./cut');
+const cardio = require('./cardio');
+const bulk = require('./bulk');
+
+Goals.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCASE'
+});
+
+User.hasMany(Workoutplan, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Workoutplan.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+cut.belongsTo(Workoutplan, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+cardio.belongsTo(Workoutplan, {
+    foreignKey: 'user_id',
+    onDelete:'CASCADE'
+});
+
+bulk.belongsTo(Workoutplan, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
 
 
 
